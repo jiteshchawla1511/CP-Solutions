cfa9cf29e5708a6747e3da2f39902db5fcb34541
Jitesh Chawla <jiteshchawla1511@gmail.com>
2022-07-26 14:04:30 +0530

    Committing files

---
+++ CodeChef/C++14/SNCKYEAR/39663918.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	while(t--)
+	{
+	    int n;
+	    cin >> n;
+	    if(n==2019 || n==2017 || n==2016 || n==2015 || n==2010)
+	    {
+	        cout<<"HOSTED"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"NOT HOSTED"<<endl;
+	    }
+	}
+	
+	
+	return 0;
+}
