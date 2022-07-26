e4c4e681026ab8997627711126ac5bfc7af439ba
Jitesh Chawla <jiteshchawla1511@gmail.com>
2022-07-26 14:04:48 +0530

    Committing files

---
+++ CodeChef/C++14/GDOG/39468287.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n,k,max=0;
+        cin >> n >> k;
+        for(int i=1;i<=k;i++){
+        if(n%i > max)
+            max = n%i;
+        }
+        cout<<max<<endl;
+    }
+}
