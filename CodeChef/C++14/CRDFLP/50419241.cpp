2abd4456503e8c9872a5b9a7373722a26abec4e9
Jitesh Chawla <jiteshchawla1511@gmail.com>
2022-07-26 14:00:56 +0530

    Committing files

---
+++ CodeChef/C++14/CRDFLP/50419241.cpp
@@ -0,0 +1,117 @@
+#include <bits/stdc++.h>
+#define ll              long long int
+#define mod             1000000007
+#define inf             1000000000000000001;
+#define mp(x,y)         make_pair(x,y)
+#define mem(a,val)      memset(a,val,sizeof(a))
+#define poss(flag)      cout << (flag ? "YES\n" : "NO\n")
+#define lld             long double
+#define ull             unsigned long long int
+using namespace std;
+#pragma GCC optimize("Ofast")
+#pragma GCC target("fma,sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native")
+#pragma GCC optimize("unroll-loops")
+//Debugger Start
+ 
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+ 
+void _print(long long int t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(long double t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(unsigned long long int t) {cerr << t;}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+ 
+// Debbuger ends
+
+void yes(){
+    cout<<"YES"<<"\n";
+}
+void no(){
+    cout<<"NO"<<"\n";
+} 
+void solve(){
+    ll n;
+    cin >> n;
+    vector<ll>f(n),b(n),state(n,-1);
+    for(int i=0;i<n;i++){
+        cin >> f[i];
+    }
+    for(int i=0;i<n;i++){
+        cin >> b[i];
+    }
+    for(ll bit = (1 << 29); bit > 0;bit >>= 1){
+        bool flag = true;
+        for(int i=0;i<n;i++){
+            if(state[i] == 0 && !(f[i] & bit)){
+                flag = false;
+            }else if(state[i] == 1 && !(b[i] & bit)){
+                flag = false;
+            }else if(!(f[i] & bit) && !(b[i] & bit)){
+                flag = false;
+            }
+        }
+        if(!flag){
+            continue;
+        }
+        for(int i=0;i<n;i++){
+            if(state[i] != -1){
+                continue;
+            }
+            if(!(f[i] & bit)){
+                state[i] = 1;
+            }else if(!(b[i] & bit)){
+                state[i] = 0;
+            }
+        }
+    }
+    ll ans = (1 << 30) - 1;
+    ll flips =0;
+    for(ll i=0;i<n;i++){
+        if(state[i] == 1){
+            flips++;
+            ans &= b[i];
+        }else{
+            ans &= f[i];
+        }
+    }
+    cout<<ans<<" "<<flips<<"\n";
+
+
+
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+#ifndef ONLINE_JUDGE
+    freopen("Error.txt", "w", stderr);
+#endif
+ 
+    int t = 1;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+ 
+ 
+ 
+#ifndef ONLINE_JUDGE
+    cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
+#endif
+    return 0;
+}
