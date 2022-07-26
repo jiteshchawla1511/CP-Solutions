d466130b975046437a91b962ef77f5c40dae38ae
Jitesh Chawla <jiteshchawla1511@gmail.com>
2022-07-26 14:04:33 +0530

    Committing files

---
+++ CodeChef/C++14/VOWELTB/39657735.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+    char ch;
+	cin >> ch;
+	if(ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+	{
+	    cout<<"Vowel"<<endl;
+	    
+	}
+	else
+	{
+	    cout<<"Consonant"<<endl;
+	    
+	}
+	
+	
+	return 0;
+}
