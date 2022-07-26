a0d71cecc8e76f9dc4710a96ed4bed02512af7ae
Jitesh Chawla <jiteshchawla1511@gmail.com>
2022-07-26 13:58:01 +0530

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc197_a/22071035.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstring>
+#include <chrono>
+#include <complex>
+#define ll              long long int
+#define vi              vector<int>
+#define vll             vector<ll>
+#define vvi             vector < vi >
+#define pb              push_back
+#define all(x)          (x).begin(),(x).end()
+#define pii             pair<int,int>
+#define vpl             vector<pair<ll,ll>>
+#define pll             pair<long long, long long>
+#define mod             1000000007
+#define inf             1000000000000000001;
+#define mp(x,y)         make_pair(x,y)
+#define mem(a,val)      memset(a,val,sizeof(a))
+#define eb              emplace_back
+#define ff              first
+#define ss              second
+#define poss(flag)      cout << (flag ? "YES\n" : "NO\n")
+#define rep(i,j,k)      for(ll i=j;i<=k;i++)
+#define repr(i,j,k)     for(ll i=j;i>=k;i--)
+#define ii              insert
+using namespace std;
+
+int sqr(int a){
+    return a*a;
+}
+int main()
+{
+    string s;
+    cin >> s;
+    swap(s[1],s[2]);
+    reverse(all(s));
+    cout<<s;
+
+    return 0;
+    
+}
