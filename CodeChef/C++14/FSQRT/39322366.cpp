40704763cd64d5693b92d2c04feaf2dc2633955a
Jitesh Chawla <jiteshchawla1511@gmail.com>
2022-07-26 14:05:14 +0530

    Committing files

---
+++ CodeChef/C++14/FSQRT/39322366.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+
+int main() {
+    int t;
+    cin >> t;
+    for(int i=0;i<t;i++)
+    {
+        long int n;
+        cin>>n;
+        cout<<int(sqrt(n))<<endl;
+       
+    }
+    
+	return 0;
+}
