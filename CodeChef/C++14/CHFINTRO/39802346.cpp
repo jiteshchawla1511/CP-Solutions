98ef46b0fc5198a8384d84f7bd6d2edfa4d2df8e
Jitesh Chawla <jiteshchawla1511@gmail.com>
2022-07-26 14:03:26 +0530

    Committing files

---
+++ CodeChef/C++14/CHFINTRO/39802346.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin >> t;
+	int n;
+	cin >> n;
+	while(t--)
+	{
+	    int x;
+	    cin >> x;
+	    if(x>=n)
+	    {
+	        cout<<"Good boi"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"Bad boi"<<endl;
+	    }
+	}
+	return 0;
+}
