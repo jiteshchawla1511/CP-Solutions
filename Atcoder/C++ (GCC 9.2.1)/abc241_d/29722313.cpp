280c5be4f1225cc04638697c15e8333ffd9c869e
Jitesh Chawla <jiteshchawla1511@gmail.com>
2022-07-26 13:55:16 +0530

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc241_d/29722313.cpp
@@ -0,0 +1,161 @@
+#include <bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp> 
+using namespace std::chrono;
+#define SPED            ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define int             long long int
+#define mod             1000000007
+#define nline           '\n' 
+#define all(x)          (x).begin(), (x).end()
+#define modulo(x,y)     ((x %y +y) % y)
+#define google(x)       cout << "Case #" << x << ": ";
+#define inf             1000000000000000001
+#define mp(x,y)         make_pair(x,y)
+#define mem(a,val)      memset(a,val,sizeof(a))
+#define poss(flag)      cout << (flag ? "YES\n" : "NO\n")
+#define lld             long double
+#define ull             unsigned int
+#define sz(x) (int)x.size()
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds; // find_by_order, order_of_key
+#pragma GCC optimize("Ofast")
+#pragma GCC target("fma,sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native")
+#pragma GCC optimize("unroll-loops")
+//Debugger Start
+ 
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+ 
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(long double t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(unsigned int t) {cerr << t;}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+ 
+// Debbuger ends
+
+int dx[] = {-1,1,0,0};
+int dy[] = {0,0,-1,1};
+void yes(int n = 0){
+    if(n == 1){
+        cout<<"Yes"<<"\n";
+    }else{
+        cout<<"YES"<<"\n";
+    }
+}
+void no(int n = 0){
+    if(n == 1){
+        cout<<"No"<<"\n";
+    }else{
+        cout<<"NO"<<"\n";
+    }
+} 
+bool cmp(pair<int,int>a,pair<int,int>b){
+    if(b.second == a.second){
+        return a.first > b.first;
+    }else{
+        return a.second > b.second;
+    }
+}
+void printvec(vector<int> &v){
+    for(int i = 0 ; i < sz(v) ; i++){
+        cout << v[i] << " ";
+    }
+}
+void readvec(vector<int> &v){
+    for(int i = 0 ; i < sz(v) ; i++){
+        cin >> v[i];
+    }
+}
+void solve() {
+    int n;
+    cin >> n;
+    multiset<int>ms;
+    for(int i = 0;i <n;i ++){
+        int q;
+        cin >>q ;
+        if(q == 1){
+            int x;
+            cin >> x;
+            ms.insert(x);
+
+        }else if(q == 3){
+
+            int x,k;
+            cin >> x >> k;
+            auto itr = ms.lower_bound(x);
+            k -= 1;
+            while(itr != ms.end() && k > 0){
+                itr ++;
+                k -= 1;
+            }
+            if(itr == ms.end()){
+                cout << -1 << nline;
+                continue;
+            }
+            if(!k){
+                cout << *itr << nline;
+            }else{
+                cout << -1 << nline;
+            }
+
+        }else {
+
+            int x,k;
+            cin >> x >> k;
+            auto itr = ms.upper_bound(x);
+            while(itr != ms.begin() && k > 0){
+                itr --;
+                k -= 1;
+            }
+            if(!k){
+                cout << *itr << nline;
+            }else{
+                cout << -1 << nline;
+            }
+            
+
+
+        }
+
+    }
+    
+    
+}
+signed main()
+{
+    auto start = high_resolution_clock::now();
+    SPED
+#ifndef ONLINE_JUDGE
+    freopen("Error.txt", "w", stderr);
+#endif
+ 
+    int t = 1;
+    // cin >> t;
+    for(int T = 1;T <= t;T ++){
+        cout << setprecision(15) << fixed ;
+        solve();
+    }
+ 
+ 
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+    cerr << "\nTime taken : " << fixed << duration.count() / 1000000.0 << "seconds" << "\n";
+    
+    return 0;
+}
