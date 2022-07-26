2a49829dcb78d49da23becadf1b30c0fd2c7f6be
Jitesh Chawla <jiteshchawla1511@gmail.com>
2022-07-26 13:59:29 +0530

    Committing files

---
+++ CodeChef/C++14/SUMPOS/42027216.cpp
@@ -0,0 +1,56 @@
+/*By jitu_falcon15*/
+#include <bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstring>
+#include <chrono>
+#include <complex>
+#define ll long long int
+#define vi vector<int>
+#define vll vector<ll>
+#define vvi vector < vi >
+#define pb push_back
+#define pii pair<int,int>
+#define vpl vector<pair<ll,ll>>
+#define pll pair<long long, long long>
+#define mod 1000000007
+#define inf 1000000000000000001;
+#define mp(x,y) make_pair(x,y)
+#define mem(a,val) memset(a,val,sizeof(a))
+#define eb emplace_back
+#define ff first
+#define ss second
+
+using namespace std;
+int main()
+{
+    std::ios::sync_with_stdio(false);
+#ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+#endif
+
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+        ll x,y,z;
+        cin >> x >> y >> z;
+        bool flag = false;
+        if(x == y+z || y == x+z || z == x+y)
+        {
+            flag = true;
+        }
+        if(flag)
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+        {
+            cout<<"NO"<<endl;
+        }
+    }
+
+    return 0;
+}
