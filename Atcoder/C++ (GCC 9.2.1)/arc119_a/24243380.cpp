858adc12e3cc10bd61383ee96891c34f3d8ada6b
Jitesh Chawla <jiteshchawla1511@gmail.com>
2022-07-26 13:57:21 +0530

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/arc119_a/24243380.cpp
@@ -0,0 +1,122 @@
+#include <bits/stdc++.h>
+#define ll              long long int
+#define vi              vector<int>
+#define vll             vector<ll>
+#define vvi             vector < vi >
+#define pb              push_back
+#define all(x)          (x).begin(),(x).end()
+#define pii             pair<int,int>
+#define vpl             vector<pair<ll,ll>>
+#define pll             pair<long long, long long>
+#define mod             1000000007
+#define inf             1000000000000000001;
+#define mp(x,y)         make_pair(x,y)
+#define mem(a,val)      memset(a,val,sizeof(a))
+#define eb              emplace_back
+#define ff              first
+#define ss              second
+#define poss(flag)      cout << (flag ? "YES\n" : "NO\n")
+#define ii              insert
+#define lld             long double
+#define ull             unsigned long long int
+using namespace std;
+#pragma GCC optimize("Ofast")
+#pragma GCC target("fma,sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native")
+#pragma GCC optimize("unroll-loops")
+//Debugger Start
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+// Debbuger ends
+
+ll power(ll x, unsigned ll y){
+    ll res=1;
+    x=x;
+    if (x==0) return 0;
+    while (y > 0)
+    {
+        if (y & 1)
+            res = (res*x);
+        y = y>>1;
+        x = (x*x);
+    }
+    return res;
+}
+int _pow(int a, int b){
+    if(!b)
+        return 1;
+    int temp = _pow(a, b / 2);
+    temp = (temp * temp);
+    if(b % 2)
+        return (a * temp);
+    return temp;
+}
+
+int cl(int a,int x){
+    if(a%x==0)
+        return a/x;
+    else
+        return a/x+1;
+}
+
+void yes(){
+    cout<<"YES"<<"\n";
+}
+void no(){
+    cout<<"NO"<<"\n";
+} 
+void solve(){
+    ll n;
+    cin >> n;
+    ll ans = INT_MAX;
+    for(int b = 0;b<=60;b++){
+        ll x = power(2,b);
+        ll a = n/x;
+        ll c = n - ((n/x) * x);
+        ans = min(ans,a + b + c);
+    }
+    cout<<ans;
+    
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+#ifndef ONLINE_JUDGE
+    freopen("Error.txt", "w", stderr);
+#endif
+
+    int t = 1;
+    // cin >> t;
+    while(t--){
+        solve();
+    }
+
+
+
+#ifndef ONLINE_JUDGE
+    cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
+#endif
+    return 0;
+}
