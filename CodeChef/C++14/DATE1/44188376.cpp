77b23a7f885901301012dfa0cc5cd1b951ed82a2
Jitesh Chawla <jiteshchawla1511@gmail.com>
2022-07-26 13:59:13 +0530

    Committing files

---
+++ CodeChef/C++14/DATE1/44188376.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cstring>
+#include <chrono>
+#include <complex>
+#define ll long long int
+#define vi vector<int>
+#define vll vector<ll>
+#define vvi vector < vi >
+#define pb push_back
+#define pii pair<int,int>
+#define vpl vector<pair<ll,ll>>
+#define pll pair<long long, long long>
+#define mod 1000000007
+#define inf 1000000000000000001;
+#define mp(x,y) make_pair(x,y)
+#define mem(a,val) memset(a,val,sizeof(a))
+#define eb emplace_back
+#define ff first
+#define ss second
+ 
+using namespace std;
+int main()
+{
+    ll t;
+    cin >> t;
+    while(t--){
+    	ll a,x,y;
+    	cin >>a >> y>>x;
+    	if(a >= y){
+    		cout<<x*y<<"\n";
+    	}else{
+    		cout<<x*a + 1<<"\n";
+    	}
+
+    }
+ 	#ifndef ONLINE_JUDGE
+  cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
+	#endif
+    return 0;
+}
