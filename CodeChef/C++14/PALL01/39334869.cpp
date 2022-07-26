46eae2c261f74609c241325b1580e2de67780352
Jitesh Chawla <jiteshchawla1511@gmail.com>
2022-07-26 14:04:59 +0530

    Committing files

---
+++ CodeChef/C++14/PALL01/39334869.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        int rev=0;
+        int temp = n;
+        while(n>0)
+        {
+            int dig = n%10;
+            rev = rev*10 + dig;
+            n /= 10;
+        }
+        if(temp == rev)
+        {
+            cout<<"wins"<<endl;
+        }
+        else
+        {
+            cout<<"loses"<<endl;
+        }
+    }
+	return 0;
+}
