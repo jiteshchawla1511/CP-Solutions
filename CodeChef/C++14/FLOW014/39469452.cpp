e5f6d181b9d231272d3c367f11f764e9acb57e83
Jitesh Chawla <jiteshchawla1511@gmail.com>
2022-07-26 14:04:44 +0530

    Committing files

---
+++ CodeChef/C++14/FLOW014/39469452.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int t;
+	cin >> t;
+	while(t--)
+	{
+	    int h,ts;
+	    float c;
+	    cin >> h >> c >> ts;
+	    
+	    
+	    if(h>50 && c<0.7 && ts>5600)
+	        cout<<10<<endl;
+	    else if(h>50 && c<0.7 )
+	        cout<<9<<endl;
+	    else if(c<0.7 && ts>5600 )
+	        cout<<8<<endl;
+	    else if(h>50 && ts>5600 )
+	        cout<<7<<endl;
+	    else if(h>50 || c<0.7 || ts>5600 )
+	        cout<<6<<endl;
+	    else
+	        cout<<5<<endl;
+	   
+	   
+	}
+	return 0;
+}
